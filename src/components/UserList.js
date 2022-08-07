5807039454e230d8e5772ff9ac6630d6c595dd7d
syifarahma <202020283@mhs.udb.ac.id>
2022-08-07 11:43:25 +0700

    update localhost

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -10,13 +10,13 @@ const UserList = () => {
   }, []);
  
   const getUsers = async () => {
-    const response = await axios.get("http://localhost:5000/users");
+    const response = await axios.get("ttps://backend-nodejs12.herokuapp.com/users");
     setUser(response.data);
   };
  
   const deleteUser = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/users/${id}`);
+      await axios.delete(`https://backend-nodejs12.herokuapp.com/users/${id}`);
       getUsers();
     } catch (error) {
       console.log(error);
